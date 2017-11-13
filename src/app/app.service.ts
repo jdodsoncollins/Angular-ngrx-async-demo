4c51037836abbf8ce9d06edb0f97ca585851b422
Jeremy Collins <jdodsoncollins@gmail.com>
2017-11-13 13:31:24 -0800

    Adding a code comment

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -6,6 +6,7 @@ import { Theater } from '../model/theater';
 @Injectable()
 export class AppService {
 
+  // require json files for treatment as network responses below
   readonly movie_metadata = require('../assets/data/movie_metadata.json');
   readonly theater_showtimes = require('../assets/data/theater_showtimes.json');
 
@@ -20,6 +21,7 @@ export class AppService {
   }
 
   public fetchMovieMetadata() {
+    // create into an Observable, so that it behaves as if it was from an API response
     return Observable.of(this.movie_metadata)
     .subscribe((res) => {
       let movies = []
